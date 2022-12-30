fb3cb844d56fea558999ec074a634f18c2d50d81
simo <simonlopezs@gmail.com>
2022-12-30 02:03:11 -0300

    game finished

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ const POKE_API = 'https://pokeapi.co/api/v2/pokemon/';
 
 let gameState
 
-function setGameState(cards) {
+function setGameState(cards = []) {
     gameState = {
-        cards: cards || [],
-        firstCardIndex: null,
-        secondCardIndex: null,
-        isLocked: false,
+        cards,
         moves: 0
     }
 }
 
-const TIMEOUT = 1500
+const TIMEOUT = 1000
 
 function getRandomNumber() {
     const randomNumber = Math.round(Math.random() * 150) + 1;
@@ -34,135 +31,16 @@ function createRandomNumbersList() {
     return numbersList;
 }
 
-
-
 function getPokemons(numbers) {
     return Promise.all(
         numbers.map(number => {
             const url = POKE_API + number
             return fetch(url)
                 .then(response => response.json())
-                .then(data => ({
-                    image: data.sprites.other['official-artwork'].front_default,
-                    id: number
-                })
+                .then(data => data.sprites.other['official-artwork'].front_default
                 )
         })
     )
-}
-
-function buildDeck(pokemons) {
-    const gridContainer = document.getElementsByClassName('grid-container')[0]
-    gridContainer.innerHTML = ''
-    const cards = shuffle(duplicate(pokemons)).map((pokemon, index) => {
-        const div = document.createElement('div')
-        const img = document.createElement('img')
-        img.src = pokemon.image
-        div.classList.add('card')
-        div.classList.add('hidden')
-        div.addEventListener('click', () => handleClick(index))
-        div.appendChild(img)
-        gridContainer.appendChild(div)
-        return {
-            node: div,
-            hidden: true,
-            paired: false,
-            ...pokemon
-        }
-    })
-    setGameState(cards)
-
-}
-
-function handleClick(index) {
-
-    const card = gameState.cards[index]
-    if (
-        card.paired
-        || gameState.firstCardIndex === index
-        || gameState.secondCardIndex === index
-    ) {
-        return;
-    }
-
-    if (gameState.isLocked) {
-        gameState.firstCardIndex = null
-        gameState.secondCardIndex = null
-        console.log('is locked');
-        gameState.cards.filter(card => !card.paired).forEach(card => card.hidden = true)
-        refreshCards()
-        gameState.isLocked = false
-    }
-    card.hidden = false
-
-    if (gameState.firstCardIndex === null) {
-        gameState.firstCardIndex = index
-    } else if (gameState.secondCardIndex === null) {
-        gameState.secondCardIndex = index
-    }
-
-    if (gameState.firstCardIndex !== null && gameState.secondCardIndex !== null) {
-        verifyPair()
-    }
-
-    refreshCards();
-    refreshMoves();
-}
-
-function verifyPair() {
-    const { cards, firstCardIndex, secondCardIndex } = gameState
-    if (cards[firstCardIndex].id === cards[secondCardIndex].id) {
-        cards[firstCardIndex].paired = true
-        cards[secondCardIndex].paired = true
-        gameState.isLocked = false
-    } else {
-        gameState.isLocked = true
-        setTimeout(() => {
-            cards[firstCardIndex].hidden = true
-            cards[secondCardIndex].hidden = true
-            gameState.isLocked = false
-            refreshCards();
-        }, TIMEOUT)
-    }
-    gameState.firstCardIndex = null
-    gameState.secondCardIndex = null
-    gameState.moves++
-}
-
-function verifyWin() {
-    if (gameState.cards.every(card => card.paired)) {
-        alert(`Ganaste en ${gameState.moves} jugadas`)
-    }
-}
-
-function refreshCards() {
-    gameState.cards.forEach(card => {
-        if (card.hidden) {
-            card.node.classList.add('hidden')
-        }
-        else {
-            card.node.classList.remove('hidden')
-        }
-        if (card.paired) {
-            card.node.classList.add('paired')
-        }
-    })
-    verifyWin();
-
-}
-
-function refreshMoves() {
-    document.getElementById('moves').innerHTML =
-        `Jugadas: ${gameState.moves}`
-}
-
-function startGame() {
-    setGameState()
-    const randomNumbers = createRandomNumbersList()
-    getPokemons(randomNumbers).then(
-        pokemons => buildDeck(pokemons)
-    )
-    refreshMoves()
 }
 
 function duplicate(list) {
@@ -172,6 +50,134 @@ function duplicate(list) {
 function shuffle(list) {
     return list.sort(() => Math.random() - 0.5)
 }
+
+function setCards(pokemons) {
+    const gridContainer = document.getElementsByClassName('grid-container')[0]
+    gridContainer.classList.add('loading')
+    gridContainer.innerHTML = ''
+    const cards = shuffle(duplicate(pokemons)).map((pokemon, index) => {
+        const div = document.createElement('div')
+        const img = document.createElement('img')
+        img.src = pokemon
+        div.classList.add('card')
+        div.classList.add('hidden')
+        div.addEventListener('click', () => handleClick(index))
+        div.appendChild(img)
+        gridContainer.appendChild(div)
+        return {
+            node: div,
+            isVisible: false,
+            isPaired: false,
+            id: pokemon
+        }
+    })
+    setGameState(cards)
+    gridContainer.classList.remove('loading')
+
+}
+
+function startGame() {
+    setGameState()
+    const randomNumbers = createRandomNumbersList()
+    getPokemons(randomNumbers).then(
+        pokemons => setCards(pokemons)
+    )
+    updateMoves()
+}
+
+
+// ----------------------------------------
+
+function showForever(cards) {
+    cards.forEach(card => {
+        showCard(card)
+        card.isPaired = true
+        card.node.classList.add('paired')
+    })
+}
+
+function showCard(card) {
+    card.isVisible = true
+    card.node.classList.remove('hidden')
+}
+
+function hideCard(card) {
+    if (card.isPaired) return;
+    card.isVisible = false
+    card.node.classList.add('hidden')
+}
+
+function hideUnpaired() {
+    const { cards } = gameState
+    cards.forEach((card) => {
+        if (!card.isPaired) {
+            hideCard(card)
+        }
+    })
+}
+
+function handleClick(index) {
+    const card = gameState.cards[index]
+    if (card.isVisible || card.isPaired) {
+        return;
+    }
+    const visibleUnpairedCards = getVisibleUnpairedCards()
+    switch (visibleUnpairedCards.length) {
+        case 0: {
+            return showCard(card)
+        }
+        case 1: {
+            showCard(card)
+            return verifyPair()
+        }
+        case 2: {
+            hideUnpaired()
+            return showCard(card)
+        }
+    }
+}
+
+
+function verifyPair() {
+    const visibleUnpairedCards = getVisibleUnpairedCards()
+    if (visibleUnpairedCards.length != 2)
+        return;
+    if (visibleUnpairedCards[0].id == visibleUnpairedCards[1].id) {
+        showForever(visibleUnpairedCards);
+    } else {
+        setTimeout(() => {
+            hideCard(visibleUnpairedCards[0])
+            hideCard(visibleUnpairedCards[1])
+        }, TIMEOUT)
+    }
+    addMove()
+    verifyWin()
+}
+
+function getVisibleUnpairedCards() {
+    const visibleUnpairedCards = gameState.cards.filter(card => card.isVisible && !card.isPaired)
+    return visibleUnpairedCards
+}
+
+function verifyWin() {
+    if (gameState.cards.every(card => card.isPaired)) {
+        setTimeout(() => alert(`Ganaste en ${gameState.moves} jugadas`), 200)
+    }
+}
+
+function addMove() {
+    gameState.moves++
+    updateMoves()
+
+}
+
+function updateMoves() {
+    document.getElementById('moves').innerHTML =
+        `Jugadas: ${gameState.moves}`
+}
+
+// ----------------------------------------
+
 
 startGame()
 
